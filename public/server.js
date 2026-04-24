0606c3326095e284d40f488c8c30a77db537825b
TechSpec71 <ritawamaa71@gmail.com>
2026-04-24 23:05:35 +0300

    Initial commit: SRT Command Center Prototype

---
+++ public/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Command Center running on http://localhost:${PORT}`);
+});
